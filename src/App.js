d8c827ca2078e567947c82b00ec0fc7d4262bcd8
Daniel <mr_schliesing@hotmail.com>
2022-03-25 21:02:18 -0800

    implemented edittext boxes mostly

--- src/App.js
+++ src/App.js
@@ -4,6 +4,10 @@ import GroupDropdown from './components/groupDropdown';
 import './stylesheets/dropdown.css'
 import LinksList from './components/LinksList';
 
+import { EditText } from 'react-edit-text';
+import 'react-edit-text/dist/index.css';
+
+
 function App() {
   
   const [groups, setGroups] = useState([{
@@ -16,57 +20,67 @@ function App() {
   //context menu: add to group
   //context menu: create new group with tabs
 
-  //create group from button press
-  //load groups 
-  //delete group 
-
-
-//Selected should jsut be an index and all updates should go to groups
-//groups[selected]
-
-
   function addGroup(group){
     //add to groups
     groups.push(group)
-    setGroups(groups)
+    setGroups([...groups])//spread old array into a new one, for setstate to recognize it needs a rerender
     localStorage.setItem('groups', JSON.stringify(groups))
     setSelected(group.length - 1)
   }
 
-  ///i dont know if this will work
   function deleteGroup(group){
-    groups = groups.filter(item => item !==group)
-    setGroups(groups)
-    localStorage.setItem('groups', JSON.stringify(groups))
+    if (selected === 0){
+      localStorage.setItem('selected', 1)
+      setSelected(1)
+    }else {
+      localStorage.setItem('selected', 0)
+      setSelected(0)
+    }
+
+    let mygroups = groups.filter(item => item !== group)
+    
+    setGroups([...mygroups])
+    localStorage.setItem('groups', JSON.stringify(mygroups))
   }
 
 
   function addlink(link){
     //add to groups
     groups[selected].links.push(link)
-    setGroups(groups)
+    setGroups([...groups])
     localStorage.setItem('groups', JSON.stringify(groups))
   }
 
   function deletelink(link){
     groups[selected].links = groups[selected].links.filter(item => item !==link)
-    setGroups(groups)
+    setGroups([...groups])
     localStorage.setItem('groups', JSON.stringify(groups))
   }
-
-
-  function loadFromJson(){
-    setGroups(JSON.parse(localStorage.getItem('groups')))
-    setSelected(JSON.parse(localStorage.getItem('selected')))
-  }
-  useEffect(()=>{loadFromJson()},[])
-
-
   const testgroup = {
     "title": "Empty Title",
     "description": "Write a description here",
     "links": []
   }
+
+  function loadFromJson(){
+
+    let fgroups = JSON.parse(localStorage.getItem('groups'))
+    let fselected = JSON.parse(localStorage.getItem('selected'))
+
+    if (fgroups === null || fselected=== null){
+    setGroups([testgroup])
+    setSelected(0)
+    localStorage.setItem('groups', JSON.stringify(groups))
+    localStorage.setItem('selection', 0)
+
+    }else{
+      setGroups(fgroups)
+      setSelected(fselected)
+    }
+    
+  }
+  useEffect(()=>{loadFromJson()},[])
+
 
   function handleAddLink(){
     
@@ -76,9 +90,26 @@ function App() {
       console.log("do nothing")
     }else {
       addlink(linkText)
+      document.getElementById("addLinkInput").value = ""
     }
   }
 
+  function groupOnSelect(group){
+    localStorage.setItem('selected', groups.indexOf(group))
+    setSelected(groups.indexOf(group))
+  }
+
+  function updateDesc(desc){
+    groups[selected].description = desc.value
+    setGroups([...groups])
+    localStorage.setItem('groups', JSON.stringify(groups))
+  }
+  function updateTitle(title){
+    console.log(title)
+    groups[selected].title = title.value
+    setGroups([...groups])
+    localStorage.setItem('groups', JSON.stringify(groups))
+  }
 
   return (
     <div className="App">
@@ -90,30 +121,38 @@ function App() {
       <button onClick={() => addGroup(testgroup)}>+</button>
         <ul>
         {
-          groups && groups.length > 0 && groups.map((group)=><li key={group.title} onClick={() => setSelected(groups.indexOf(group))}>{group.title}</li>)
+          groups && groups.length > 0 && groups.map((group)=><li key={group.title} onClick={() => groupOnSelect(group)}>{group.title}</li>)
         }
         </ul>
       </div>
+
+
       <div className='header-list'>
         <div className='title'>
-        <p>
-          <strong>
-          {(groups[selected] && groups[selected].title.length > 0) ? groups[selected].title : "undefined"}
-          </strong>
-          {(groups[selected] && groups[selected].description.length > 0) ? groups[selected].description: "undefined"}
-          </p>
+          <EditText
+            name="titleText"
+            onSave={updateTitle}
+            showEditButton
+            defaultValue={(groups[selected] && groups[selected].title.length > 0) ? groups[selected].title : "undefined"}
+          />
+          <EditText
+            name="descriptionText"
+            onSave={updateDesc}
+            showEditButton
+            defaultValue={(groups[selected] && groups[selected].description.length > 0) ? groups[selected].description: "undefined"}
+          />
         </div>
       </div>
+
+
       <nav className='links-list'>
-
           <div className='input_button_group'><button className='addLink' onClick={handleAddLink}>+</button><input id="addLinkInput"/></div>
-
           <LinksList
             links = {(groups[selected] && groups[selected].links.length > 0) ? groups[selected].links: ["undefined"]}
             deletelinkprops = {deletelink}
           />
-          <div>delete group</div >
-        </nav>
+          <div><button onClick={() => deleteGroup(groups[selected])}>delete group</button></div>
+      </nav>
 
     </div>
   );
