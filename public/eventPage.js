a8167d57e22702dbd700d2abf3cdac78a25d32a2
Daniel <mr_schliesing@hotmail.com>
2022-04-01 11:16:41 -0800

    got url to show in event page, also f mv3 and f google

--- public/eventPage.js
+++ public/eventPage.js
@@ -5,17 +5,22 @@ var contextMenuItem ={
     "contexts": ['page']  /* to make this appear only when user selects something on page */
 }
 
-
+//let queryOptions = { active: true, currentWindow: true };
+//let [tab] = await chrome.tabs.query(queryOptions);
+async function getCurrentTab() {
+    chrome.tabs.query({
+        active: true,
+        currentWindow: true
+    }, function(tabs) {
+        var tabURL = tabs[0].url;
+        alert(tabURL);
+    });
+}
 
 chrome.contextMenus.create(contextMenuItem);
 
 chrome.contextMenus.onClicked.addListener( (clickData) => {
     if(clickData.menuItemId == "tutorial02"){
-        chrome.tabs.query({active: true, lastFocusedWindow: true}, tabs => {
-            let url = tabs[0].url;
-            alert("tab url is:", url)
-            //localStorage.setItem('currentUrl', JSON.stringify(url))
-            // use `url` here inside the callback because it's asynchronous!
-        });
+        getCurrentTab()
     }
 })
